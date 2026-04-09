b902cc4be7b58348370e5014aa9f6f7cafe0bbce
Uomocosa <maggiori.samuele@gmail.com>
2026-04-09 15:03:31 +0200

    working

--- paper_scraper/extract_refs.py
+++ paper_scraper/extract_refs.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 import paper_scraper
-from paper_scraper.__global__ import SEED_DIR, REPO_DIR
+from paper_scraper.__global__ import SEED_DIR, EXTRACTED_REFERENCES
 from paper_scraper import Grobid
 
 from loguru import logger
@@ -21,7 +21,7 @@ def extract_refs():
             logger.info(f"Extracted {len(refs)} references from {pdf_file.name}.")
             all_references.extend(refs)
 
-    with open(REPO_DIR / "extracted_references.json", "w", encoding="utf-8") as f:
+    with open(EXTRACTED_REFERENCES, "w", encoding="utf-8") as f:
         json.dump(all_references, f, indent=4)
 
     logger.info(
